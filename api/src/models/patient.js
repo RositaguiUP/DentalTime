ee0e44d7a397e42b86b24dc02c04bf833a63403c
RositaguiUP <0225352@up.edu.mx>
2020-12-02 14:00:38 -0600

    api basics

---
+++ api/src/models/patient.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { stringify } = require('querystring');
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    director: String,
+    resume: String,
+    description: String,
+    rating: String,
+    genre: String,
+    language: String,
+    producer: String,
+    writer: String,
+    releaseDate: String,
+    streamingRelease: String,
+    earnings: String,
+    duration: String,
+    company: String
+});
+
+const Movie = mongoose.model('Movies', movieSchema);
+
+module.exports = Movie;
