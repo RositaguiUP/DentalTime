f71b14ab6d83d7fd9274c40b1ff9310877b5fe85
Charlie <carlitosgarcia2000@gmail.com>
2020-12-03 00:00:04 -0600

    navbar test

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -19,12 +19,12 @@ class Header extends Component {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#features">Featusssres</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
-                  Another action
+                  Another actions
                 </NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">
                   Something
